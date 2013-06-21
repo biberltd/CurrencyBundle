e2b01c0ab44f9e5d4c5d804d45a942000cedf4f0
Murat Ünal <murat.unal@biberltd.com>
2013-06-21 11:58:34 +0300

    1 currecy added

---
+++ Drivers/Currencies/ANGCurrency.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @name        ANGCurrency
+ * @package		BiberLtd\CurrencyBundle
+ *
+ * @author		Murat Ünal
+ * @version     1.0.0
+ * @date        21.06.2013
+ *
+ * @copyright   Biber Ltd. (http://www.biberltd.com)
+ * @license     GPL v3.0
+ *
+ * @description Initializes Netherlands Antilles Guildes.
+ *
+ */
+namespace BiberLtd\Bundles\CurrencyBundle\Drivers\Currencies;
+
+use BiberLtd\Bundles\CurrencyBundle\Drivers;
+
+class ANGCurrency extends Drivers\CurrencyDriver {
+    /**
+     * @name 			__construct()
+     *  				Constructor function.
+     *
+     * @since			1.0.0
+     * @version         1.0.0
+     * @author          Murat Ünal
+     *
+     */
+    public function __construct(){
+        $currency_config = array(
+            'code'      => substr(__CLASS__,0, 3),
+            'name'      => 'Netherlands Antilles Guildes',
+            'symbol'    => '',
+        );
+        parent::__construct($currency_config);
+    }
+}
+/**
+ * Change Log:
+ * **************************************
+ * v1.0.0                      Murat Ünal
+ * **************************************
+ * A __construct()
+ * 
+ */
